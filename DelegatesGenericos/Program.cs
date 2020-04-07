10763a429e4bd32f2e8753fd526c36dce32c563e
Felipe Tavares <felipe.tavares@outlook.com>
2020-04-07 20:32:17 -0300

    Utilizando generics em conjunto com delegates

---
+++ DelegatesGenericos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesGenericos
+{
+    class Program
+    {
+        delegate T CalculoMatematico<T>(T a, T b);
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            CalculoMatematico<int> calculo = new CalculoMatematico<int>(p.Somar);
+            Console.WriteLine(calculo(1, 1));
+            ExibirInformacoesDelegate(calculo);
+            Console.ReadKey();
+        }
+
+        private static void ExibirInformacoesDelegate<T>(CalculoMatematico<T> calculo)
+        {
+            Console.WriteLine(calculo.Target);
+            Console.WriteLine(calculo.Method);
+        }
+
+        int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        decimal Somar(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
