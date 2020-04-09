d8c09cf9bd7bb5cb9642d4b86778971f82aedf95
Felipe Tavares <felipe.tavares@outlook.com>
2020-04-09 19:49:44 -0300

    Delegates Func e Action

---
+++ DelegatesFuncAction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesFuncAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Func<int, int, int> calculo = p.Somar;
+            Action<int> impressao = p.ImprimirResultado;
+            impressao(calculo(1, 1));
+            Console.ReadKey();
+        }
+
+        void ImprimirResultado(int resultado)
+        {
+            Console.WriteLine(resultado);
+        }
+
+        int Somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
