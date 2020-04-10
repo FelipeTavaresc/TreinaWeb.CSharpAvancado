688ee8e8e69bae9ee8f550f4212d3f98a8b24688
Felipe Tavares <felipe.tavares@outlook.com>
2020-04-10 12:20:07 -0300

    Orientação a objetos com orientação a eventos

---
+++ Eventos/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eventos
+{
+    public partial class frmGerenciadorLatidosCachorros : Form
+    {
+        private GerenciadorLatidos _gerenciadorLatidos;
+
+        public frmGerenciadorLatidosCachorros()
+        {
+            InitializeComponent();
+            _gerenciadorLatidos = new GerenciadorLatidos();
+            _gerenciadorLatidos.ExcessoDecibeisEvent += QuandoExcederDecibeis;
+            _gerenciadorLatidos.ExcessoDecibeisEvent += QuandoExcederDecibeisNovamente;
+        }
+
+        private void frmGerenciadorLatidosCachorros_Load(object sender, EventArgs e)
+        {
+            pgbIntensidadeLatido.Value = 0;
+        }
+
+        private void btnLatir_Click(object sender, EventArgs e)
+        {
+            pgbIntensidadeLatido.Value = _gerenciadorLatidos.Latir(); 
+        }
+
+        private void QuandoExcederDecibeis(Object sender, EventArgs e)
+        {
+            MessageBox.Show("O animal passou dos limites", "Excesso de decibéis", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void QuandoExcederDecibeisNovamente(Object sender, EventArgs e)
+        {
+            MessageBox.Show("Você receberá uma multa", "Excesso de decibéis", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
---
+++ Eventos/GerenciadorLatidos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventos
+{
+    public class GerenciadorLatidos
+    {
+        private int _intesidadeLatido;
+        public delegate void ExcessoDecibeisHandler(object sender, EventArgs e);
+        public event ExcessoDecibeisHandler ExcessoDecibeisEvent;
+
+        public GerenciadorLatidos()
+        {
+            _intesidadeLatido = 0;
+        }
+
+        public int Latir()
+        {
+            _intesidadeLatido += 10;
+            if (_intesidadeLatido > 80)
+            {
+                OnExcessoDecibeis(new EventArgs());
+            }
+            return _intesidadeLatido;
+        }
+
+        protected virtual void OnExcessoDecibeis(EventArgs e)
+        {
+            if (ExcessoDecibeisEvent != null)
+            {
+                ExcessoDecibeisEvent(this, e); 
+            }
+        }
+    }
+}
