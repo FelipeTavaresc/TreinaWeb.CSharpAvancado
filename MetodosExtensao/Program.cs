540be464ea711e68064d7d5ad8d6f024c7cff3ce
Felipe Tavares <felipe.tavares@outlook.com>
2020-04-15 20:06:20 -0300

    Criando e utilizando métodos de extensão

---
+++ MetodosExtensao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type what you want to invert: ");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Your new phase is {0} ", phrase.InverterCaixas(false));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MetodosExtensao/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtensao
+{
+    public static class StringExtensions
+    {
+        public static string InverterCaixas(this string value, bool initialState)
+        {
+            bool isUpperCase = initialState;
+            string result = string.Empty;
+            for (int i = 0; i < value.Length; i++)
+            {
+                result += isUpperCase ? value[i].ToString().ToUpper() : value[i].ToString().ToLower();
+                isUpperCase = !isUpperCase;
+            }
+            return result;
+        }
+    }
+}
